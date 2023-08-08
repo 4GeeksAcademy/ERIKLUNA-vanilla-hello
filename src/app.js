af531d3bb7f8ced28fc3c2c5545c5f1a6c4873e5
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2023-08-08 22:50:57 +0000

    nuevo codigo

--- src/app.js
+++ src/app.js
@@ -6,39 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  document.querySelector("#TextoExcusa").innerHTML = generarExcusa();
+  document.querySelector("#TextoExcusa").innerHTML=excusa();
 };
 
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
 
-function arrayAleatorio(array) {
-  const NuevoArray = Math.floor(Math.random() * array.length);
-  return array[NuevoArray];
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+
+function excusa () {
+
+  for (let i = 0; i < who.length; i++) {
+      const aleatorioWho = Math.floor(Math.random() * who.length);
+      for (let j = 0; j < action.length; j++) {
+          const aleatorioAction = Math.floor(Math.random() * action.length);
+        for (let k = 0; k < what.length; k++) {
+          const aleatorioWhat = Math.floor(Math.random() * what.length);
+          for (let l = 0; l < when.length; l++) {
+              const aleatorioWhen = Math.floor(Math.random() * when.length);
+
+              const generarExcusa = `${who[aleatorioWho]} ${action[aleatorioAction]} ${what[aleatorioWhat]} ${when[aleatorioWhen]}`;
+          return generarExcusa;
+        }
+      }
+    }
+  }
+
 }
 
-function generarExcusa() {
-  const aleatorioWho = arrayAleatorio(who);
-  const aleatorioAction = arrayAleatorio(action);
-  const aleatorioWhat = arrayAleatorio(what);
-  const aleatorioWhen = arrayAleatorio(when);
-
-  const excuse =
-    aleatorioWho +
-    " " +
-    aleatorioAction +
-    " " +
-    aleatorioWhat +
-    " " +
-    aleatorioWhen;
-
-  return excuse;
-}
