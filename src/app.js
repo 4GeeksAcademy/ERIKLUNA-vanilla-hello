e6b171df4c6b991d6379a0a8b0d771c129979dd1
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2023-08-08 21:16:02 +0000

    codigo  definitivo

--- src/app.js
+++ src/app.js
@@ -6,42 +6,39 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#TextoExcusa").innerHTML = generarExcusa();
+};
 
-  window.onload = function() {
-    document.querySelector("#TextoExcusa").innerHTML = generarExcusa();
-  };
-  
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying",
-  ];
-  
-  function arrayAleatorio(array) {
-    const NuevoArray = Math.floor(Math.random() * array.length);
-    return array[NuevoArray];
-  }
-  
-  function generarExcusa() {
-    const aleatorioWho = arrayAleatorio(who);
-    const aleatorioAction = arrayAleatorio(action);
-    const aleatorioWhat = arrayAleatorio(what);
-    const aleatorioWhen = arrayAleatorio(when);
-  
-    const excuse =
-      aleatorioWho +
-      " " +
-      aleatorioAction +
-      " " +
-      aleatorioWhat +
-      " " +
-      aleatorioWhen;
-  
-    return excuse;
-  };
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function arrayAleatorio(array) {
+  const NuevoArray = Math.floor(Math.random() * array.length);
+  return array[NuevoArray];
+}
+
+function generarExcusa() {
+  const aleatorioWho = arrayAleatorio(who);
+  const aleatorioAction = arrayAleatorio(action);
+  const aleatorioWhat = arrayAleatorio(what);
+  const aleatorioWhen = arrayAleatorio(when);
+
+  const excuse =
+    aleatorioWho +
+    " " +
+    aleatorioAction +
+    " " +
+    aleatorioWhat +
+    " " +
+    aleatorioWhen;
+
+  return excuse;
+}
