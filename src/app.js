85c9abac9df3362659f738f38e3afe0e656ae3c8
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2023-08-15 19:25:05 +0000

    codigo modificado

--- src/app.js
+++ src/app.js
@@ -6,33 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  document.querySelector("#TextoExcusa").innerHTML=excusa();
+  document.querySelector("#TextoExcusa").innerHTML = excusa();
 };
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
-
-
-function excusa () {
-
+function excusa() {
   for (let i = 0; i < who.length; i++) {
-      const aleatorioWho = Math.floor(Math.random() * who.length);
-      for (let j = 0; j < action.length; j++) {
-          const aleatorioAction = Math.floor(Math.random() * action.length);
-        for (let k = 0; k < what.length; k++) {
-          const aleatorioWhat = Math.floor(Math.random() * what.length);
-          for (let l = 0; l < when.length; l++) {
-              const aleatorioWhen = Math.floor(Math.random() * when.length);
+    const aleatorioWho = Math.floor(Math.random() * who.length);
+    for (let j = 0; j < action.length; j++) {
+      const aleatorioAction = Math.floor(Math.random() * action.length);
+      for (let k = 0; k < what.length; k++) {
+        const aleatorioWhat = Math.floor(Math.random() * what.length);
+        for (let l = 0; l < when.length; l++) {
+          const aleatorioWhen = Math.floor(Math.random() * when.length);
 
-              const generarExcusa = `${who[aleatorioWho]} ${action[aleatorioAction]} ${what[aleatorioWhat]} ${when[aleatorioWhen]}`;
+          const generarExcusa = `${who[aleatorioWho]} ${action[aleatorioAction]} ${what[aleatorioWhat]} ${when[aleatorioWhen]}`;
           return generarExcusa;
         }
       }
     }
   }
-
 }
-
--- vanilla.js
+++ vanilla.js
@@ -1,30 +1,27 @@
 window.onload = function() {
-  document.querySelector("#TextoExcusa").innerHTML=excusa();
+  document.querySelector("#TextoExcusa").innerHTML = generarExcusa();
 };
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying",
+];
 
-let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
-
-
-function excusa () {
-
-  for (let i = 0; i < who.length; i++) {
-      const aleatorioWho = Math.floor(Math.random() * who.length);
-      for (let j = 0; j < action.length; j++) {
-          const aleatorioAction = Math.floor(Math.random() * action.length);
-        for (let k = 0; k < what.length; k++) {
-          const aleatorioWhat = Math.floor(Math.random() * what.length);
-          for (let l = 0; l < when.length; l++) {
-              const aleatorioWhen = Math.floor(Math.random() * when.length);
-
-              const generarExcusa = `${who[aleatorioWho]} ${action[aleatorioAction]} ${what[aleatorioWhat]} ${when[aleatorioWhen]}`;
-          return generarExcusa;
-        }
-      }
-    }
-  }
+function generarNumeroAleatorio(maxLength) {
+  return Math.floor(Math.random() * maxLength);
 }
 
+const whoAleatorio = generarNumeroAleatorio(who.length);
+const actionAleatorio = generarNumeroAleatorio(action.length);
+const whatAleatorio = generarNumeroAleatorio(what.length);
+const whenAleatorio = generarNumeroAleatorio(when.length);
+
+const generarExcusa = `${who[whoAleatorio]} ${action[actionAleatorio]} ${what[whatAleatorio]} ${when[whenAleatorio]}`;
+
+return generarExcusa;
